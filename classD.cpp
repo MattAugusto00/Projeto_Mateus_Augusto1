7c6371f4fbf1bbd5bad48638fc88cec3d22ddb5c
Mateus Augusto <mateus.augusto9@outlook.com>
2022-02-06 13:31:42 -0300

    Criação da Classe D

---
+++ classD.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+class D{
+public:
+  D(int D1, int D2);
+
+  void MD1();
+  void MD2();
+private:
+  int D1;
+  int D2;
+};
+
+D::D(int D1, int D2){
+  this->D1=D1;
+  this->D2=D2;
+}
+
+void D::MD1(){
+  cout << "MD1()" << endl;
+}
+void D::MD2(){
+  cout << "MD2()" << endl;
+}
+
+int main(){
+
+return 0;
+}
